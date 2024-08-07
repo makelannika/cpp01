ddde231a4390f0269d4fa1e188aeff43c22d410f
makelannika <makelannika@gmail.com>
2024-08-07 12:50:05 +0300

    cleaned

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 02:05:05 by amakela           #+#    #+#             */
-/*   Updated: 2024/08/05 02:19:55 by amakela          ###   ########.fr       */
+/*   Updated: 2024/08/07 12:46:28 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,15 @@
 int main()
 {
     std::string str = "HI THIS IS BRAIN";
-    std::string *pstr = &str;
-    std::string &rstr = str;
+    std::string *str_ptr = &str;
+    std::string &str_ref = str;
 
     std::cout << "address of str variable: " << &str << std::endl;
-    std::cout << "address held by str pointer: " << pstr << std::endl;
-    std::cout << "address held by str reference: " << &rstr << std::endl;
+    std::cout << "address held by str pointer: " << str_ptr << std::endl;
+    std::cout << "address held by str reference: " << &str_ref << std::endl;
+	std::cout << std::endl;
     std::cout << "value of str variable: " << str << std::endl;
-    std::cout << "value pointed to by str pointer: " << *pstr << std::endl;
-    std::cout << "value pointed to by str reference: " << rstr << std::endl;
+    std::cout << "value pointed to by str pointer: " << *str_ptr << std::endl;
+    std::cout << "value pointed to by str reference: " << str_ref << std::endl;
     return (0);
 }
