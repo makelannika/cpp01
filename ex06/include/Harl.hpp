e4b0a78806fb694c6ba6943c57ade65c056f1bb2
makelannika <makelannika@gmail.com>
2024-08-30 22:10:33 +0300

    ex06

--- ex06/include/Harl.hpp
+++ ex06/include/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 12:13:15 by amakela           #+#    #+#             */
-/*   Updated: 2024/08/14 12:37:35 by amakela          ###   ########.fr       */
+/*   Updated: 2024/08/30 21:48:50 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ class Harl {
         void    info( void );
         void    warning( void );
         void    error( void );
+
+		enum {DEBUG, INFO, WARNING, ERROR};
 
     public:
         void    complain( char* level );
--- ex06/src/Harl.cpp
+++ ex06/src/Harl.cpp
@@ -6,56 +6,64 @@
 /*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 12:13:20 by amakela           #+#    #+#             */
-/*   Updated: 2024/08/14 22:13:03 by amakela          ###   ########.fr       */
+/*   Updated: 2024/08/30 22:07:05 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Harl.hpp"
 
 void    Harl::debug( void ) {
-    std::cout << "I love having extra bacon for my"
-    " 7XL-double-cheese-triple-pickle-specialketchup burger."
-    " I really do!" << std::endl;
+    std::cout << "[ DEBUG ]" << std::endl << "I love having extra bacon for my"
+    " 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
 void    Harl::info( void ) {
-    std::cout << "I cannot believe adding extra bacon costs more money."
-    " You didn’t put enough bacon in my burger!"
-    " If you did, I wouldn’t be asking for more!" << std::endl;
+    std::cout << "[ INFO ]" << std::endl << "I cannot believe adding extra bacon costs more money."
+    << std::endl << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void    Harl::warning( void ) {
-    std::cout << "I think I deserve to have some extra bacon for free."
-    " I’ve been coming for years whereas you started working here since last month."
-    << std::endl;
+    std::cout << "[ WARNING ]" << std::endl << "I think I deserve to have some extra bacon for free."
+    << std::endl << "I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void    Harl::error( void ) {
-    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+    std::cout << "[ ERROR ]" << std::endl << "This is unacceptable, "
+	"I want to speak to the manager now." << std::endl;
 }
 
 void    Harl::complain( char* level ) {
-    switch(level[0]) {
-        case 'D':
-            debug();
-            info();
-            warning();
-            error();
+	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void (Harl::*complaints[])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	
+	for (int i = 0; i < 4; i++)
+		if (level == levels[i]) {
+			
+    switch(i) {
+        case DEBUG:
+			for (int i = 0; i < 4; i++) {
+				(this->*complaints[i])();
+				std::cout << std::endl;
+			}
             break;
-        case 'I':
-            info();
-            warning();
-            error();
+        case INFO:
+			for (int i = 1; i < 4; i++) {
+				(this->*complaints[i])();
+				std::cout << std::endl;
+			}
             break;
-        case 'W':
-            warning();
-            error();
+        case WARNING:
+			for (int i = 2; i < 4; i++) {
+				(this->*complaints[i])();
+				std::cout << std::endl;
+			}
             break;
-        case 'E':
-            error();
+        case ERROR:
+            (this->*complaints[i])();
+			std::cout << std::endl;
             break;
-        default:
-            std::cout << "Probably complaining about insignificant problems" << std::endl;
-            break;
+        return ;
+		}
     }
+	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 }
