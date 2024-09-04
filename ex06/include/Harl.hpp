875681477186fd7aad64796664b5495eceea9a9f
makelannika <makelannika@gmail.com>
2024-09-04 20:16:12 +0300

    ex06

--- ex06/include/Harl.hpp
+++ ex06/include/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 12:13:15 by amakela           #+#    #+#             */
-/*   Updated: 2024/09/04 13:53:28 by amakela          ###   ########.fr       */
+/*   Updated: 2024/09/04 20:15:45 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ class Harl {
     public:
         Harl();
         ~Harl();
+		
         void    complain( char* level );
 };
 
